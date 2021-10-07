16a6045490818280a5ec3a6f57956c162c39e6eb
racarlosdavid <2111724640101@ingenieria.usac.edu.gt>
2021-10-07 17:34:06 -0600

    add code

---
+++ src/src.ino
@@ -0,0 +1,31 @@
+#include "SR04.h"
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long a;
+
+long distanciaDeActivacion = 10; 
+int RELAY_PIN = 10; //Pin al que esta conectado el relay
+
+void setup() {
+   pinMode(RELAY_PIN, OUTPUT); // pin 10 como salida 
+   Serial.begin(9600);
+   delay(1000);
+}
+
+void loop() {
+   a=sr04.Distance();
+   Serial.print(a);
+   Serial.println("cm");
+   if (a <= distanciaDeActivacion){
+    Serial.println("despachar producto");
+    despacharProducto(3000); //Si el sensor detecta un objeto a menos de 10 cm se activa
+  }
+   delay(1000);
+}
+
+void despacharProducto(int tiempo) {
+  digitalWrite(RELAY_PIN, HIGH); // RELAY_PIN a uno logico
+  delay(tiempo); //Tiempo que voy a dejar la bomba encendida para que despache el producto
+  digitalWrite(RELAY_PIN, LOW);  // RELAY_PIN a cero logico
+}
